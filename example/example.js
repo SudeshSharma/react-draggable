c08a344943b9a333de1b1324c61d07e8bd48b7bf
Sudesh Sharma <43905979+SudeshSharma@users.noreply.github.com>
2019-01-16 14:29:33 +0530

    Update example.js
    what to check the output

--- example/example.js
+++ example/example.js
@@ -108,7 +108,7 @@ var App = React.createClass({
         </Draggable>
         <div className="box" style={{height: '500px', width: '500px', position: 'relative', overflow: 'auto', padding: '0'}}>
           <div style={{height: '1000px', width: '1000px', padding: '10px'}}>
-            <Draggable bounds="parent" {...dragHandlers}>
+            <Draggable  {...dragHandlers}>
               <div className="box">
                 I can only be moved within my offsetParent.<br /><br />
                 Both parent padding and child margin work properly.
